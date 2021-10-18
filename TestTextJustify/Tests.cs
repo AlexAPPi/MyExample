1b6eda4817981d59b96df95b46854762e5da55d2
Шамарин Александр <alexsandrshamarin@yandex.ru>
2021-10-18 14:00:00 +0300

    Добавьте файлы проекта.

---
+++ TestTextJustify/Tests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace TestTextJustify
+{
+    class Tests
+    {
+        public static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[] { new[] { "xxx" }, 3, "xxx" }; //Одно слово без пробелов
+            yield return new object[] { new[] { "xxx" }, 4, "xxx " }; //Одно слово с одним пробелом
+            yield return new object[] { new[] { "xxx" }, 7, "xxx    " }; //Одно слово с пробелами
+
+            yield return new object[] { new[] { "x", "y" }, 3, "x y" }; //Два слова, один пробел
+            yield return new object[] { new[] { "x", "y", "z" }, 5, "x y z" }; //Три слова, по одному пробелу
+            yield return new object[] { new[] { "x", "y", "z", "w" }, 7, "x y z w" }; //Четыре слова, по одному пробелу
+
+            yield return new object[] { new[] { "x", "y" }, 4, "x  y" }; //Два слова, два пробела
+            yield return new object[] { new[] { "x", "y", "z" }, 7, "x  y  z" }; //Три слова, по два пробела
+            yield return new object[] { new[] { "x", "y", "z" }, 13, "x     y     z" }; //Три слова, по пять пробелов
+            yield return new object[] { new[] { "x", "y", "z", "w" }, 10, "x  y  z  w" }; //Четыре слова, по два пробела
+
+            yield return new object[] { new[] { "x", "y" }, 5, "x   y" }; //Два слова, три пробела
+            yield return new object[] { new[] { "x", "y", "z" }, 9, "x   y   z" }; //Три слова, по три пробела
+            yield return new object[] { new[] { "x", "y", "z", "w" }, 13, "x   y   z   w" }; //Четыре слова, по три пробела
+
+            yield return new object[] { new[] { "x", "y", "z" }, 8, "x   y  z" }; //Три слова, по два+ пробела
+            yield return new object[] { new[] { "x", "y", "z", "w" }, 11, "x   y  z  w" }; //Четыре слова, по два+ пробела
+            yield return new object[] { new[] { "x", "y", "z", "w" }, 12, "x   y   z  w" }; //Четыре слова, по два++ пробела
+
+            yield return new object[] { new[] { "x", "yyyyyyyyyy", "z" }, 14, "x yyyyyyyyyy z" }; //Длинное и два коротких
+
+            yield return new object[] { new[] { "abcd", "efg", "hijkl", "mn" }, 14 + 3, "abcd efg hijkl mn" }; //Разные слова, по одному пробелу
+            yield return new object[] { new[] { "abcd", "efg", "hijkl", "mn" }, 14 + 4, "abcd  efg hijkl mn" }; //Разные слова, по одному+ пробелу
+            yield return new object[] { new[] { "abcd", "efg", "hijkl", "mn" }, 14 + 5, "abcd  efg  hijkl mn" }; //Разные слова, по одному++ пробелу
+            yield return new object[] { new[] { "abcd", "efg", "hijkl", "mn" }, 14 + 6, "abcd  efg  hijkl  mn" }; //Разные слова, по два пробела
+            yield return new object[] { new[] { "abcd", "efg", "hijkl", "mn" }, 14 + 7, "abcd   efg  hijkl  mn" }; //Разные слова, по два+ пробела
+            yield return new object[] { new[] { "abcd", "efg", "hijkl", "mn" }, 14 + 8, "abcd   efg   hijkl  mn" }; //Разные слова, по два++ пробела
+            yield return new object[] { new[] { "abcd", "efg", "hijkl", "mn" }, 14 + 9, "abcd   efg   hijkl   mn" }; //Разные слова, по три пробела
+
+            yield return new object[] { new[] { "xxx", "xxx", "xxx", "xxx" }, 12 + 3, "xxx xxx xxx xxx" }; //одинаковые слова xxx
+            yield return new object[] { new[] { "xxx", "xxx", "yyy", "yyy" }, 12 + 3, "xxx xxx yyy yyy" }; //одинаковые слова xxx yyy
+            yield return new object[] { new[] { "xxx", "xxx", "xxx", "xxx" }, 12 + 5, "xxx  xxx  xxx xxx" }; //одинаковые слова xxx неравномерно
+            yield return new object[] { new[] { "xxx", "xxx", "yyy", "yyy" }, 12 + 5, "xxx  xxx  yyy yyy" }; //одинаковые слова xxx yyy неравномерно
+
+            yield return new object[] { new[] { "x", "y", "z", "t", "a", "b", "c" }, 16, "x  y  z  t a b c" }; //Много промежутков
+            yield return new object[] { new[] { "x", "y", "z", "t", "a", "b", "c" }, 17, "x  y  z  t  a b c" }; //Много промежутков
+            yield return new object[] { new[] { "x", "y", "z", "t", "a", "b", "c" }, 18, "x  y  z  t  a  b c" }; //Много промежутков
+            yield return new object[] { new[] { "x", "y", "z", "t", "a", "b", "c" }, 24, "x   y   z   t   a   b  c" }; //Много промежутков
+        }
+
+        [TestCaseSource(nameof(TestCases))]
+        public void Test(string[] input, int length, string expected)
+        {
+            var textJustifier = new TextJustifier();
+            var actual = textJustifier.Justify(input, length);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ TestTextJustify/TextJustifier.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTextJustify
+{
+    /* Задача:
+     * Необходимо реализовать выравнивание текста по ширине в методе Justify класса TextJustifier
+     * 
+     * Выравнивание строки по ширине достигается расстановкой пробелов между словами по следующим правилам:
+     * - Между каждым словом должен стоять хотя бы один пробел
+     * - Пробелы в начале строки должны отсутствовать
+     * - Пробелы в конце строки должны отсутствовать
+     *      Исключение: строка из одного слова добивается пробелами до конца
+     *      пример: "a    "
+     * - Пробелы распределяются равномерно
+     *      пример: "a  b  c  d  e"
+     * - Если равномерное распределение пробелов невозможно,
+     *      то "лишние" пробелы добавляются по одному в интервалы начиная с крайнего левого
+     *      пример: "a   b   c  d  e"
+     *
+     * Входные данные:
+     * - текст - массив слов, состоящий из латинских букв и цифр, БЕЗ пробельных символов, БЕЗ переносов строк
+     * - длина результирующей строки - целое неотрицательное число,
+     *      достаточно большое, чтобы между каждым словом в результирующей строке вместилось хотя бы по одному пробелу
+     * 
+     * Пример использования:
+     * var result = new TextJustifier().Justify(new [] {"Xxx", "yyy", "123"}, 20);
+     * 
+     * Для проверки решения необходимо запустить тесты.
+     */
+    public class TextJustifier
+    {
+        /// <summary>
+        /// Return list of space char
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerable<char> ListOfSpaceChar(int size)
+        {
+            int count = 0;
+            while(count++ < size)
+            {
+                yield return ' ';
+            }
+        }
+
+        /// <summary>
+        /// Return list of space char
+        /// </summary>
+        /// <returns></returns>
+        private char[] ListOfSpaceCharNative(int size)
+        {
+            char[] result = new char[size];
+            for(int i = 0; i < size; i++)
+            {
+                result[i] = ' ';
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Generate string of space chars
+        /// </summary>
+        /// <param name="l">Result string length</param>
+        /// <returns></returns>
+        private string GenerateSpaceString(int length)
+        {
+            IEnumerable<char> spaces = ListOfSpaceChar(length);
+            return new string(spaces.ToArray());
+        }
+
+        /// <summary>
+        /// Generate string of space chars
+        /// </summary>
+        /// <param name="l">Result string length</param>
+        /// <returns></returns>
+        private string GenerateSpaceStringNative(int length)
+        {
+            return new string(ListOfSpaceCharNative(length));
+        }
+
+        /// <summary>
+        /// Calculate array of string lengths
+        /// </summary>
+        /// <param name="stringArray"></param>
+        /// <returns></returns>
+        private int CalcStringsLength(string[] stringArray) =>
+            stringArray.Sum(someString => someString.Length);
+
+        /// <summary>
+        /// Calculate array of string lengths
+        /// </summary>
+        /// <param name="stringArray"></param>
+        /// <returns></returns>
+        private int CalcStringsLengthNative(string[] stringArray)
+        {
+            int result = 0;
+            for(int i = 0; i < stringArray.Length; i++)
+            {
+                result += stringArray[i].Length;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Convert string array to stringbuilder array
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="l">Max capacity</param>
+        /// <returns></returns>
+        private StringBuilder[] GetStringBuilders(string[] x)
+        {
+            int length = x.Length;
+            StringBuilder[] result = new StringBuilder[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = new StringBuilder(x[i]);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Justify string array to one string for length
+        /// </summary>
+        /// <param name="x">String array for justify</param>
+        /// <param name="l">Result string length</param>
+        /// <returns></returns>
+        public string Justify(string[] x, int l)
+        {
+            int arrayLength = x.Length;
+#if UseNative
+            int stringsLength = CalcStringsLengthNative(x);
+#else
+            int stringsLength = CalcStringsLength(x);
+#endif
+            if (stringsLength > l)
+            {
+                throw new ArgumentException($"The length of the resulting string is less than the lengths of the concatenated strings");
+            }
+
+            if (arrayLength == 1)
+            {
+                string firstString = x[0];
+                if (stringsLength == l)
+                {
+                    return firstString;
+                }
+
+#if UseNative
+                string sep = GenerateSpaceStringNative(l - firstString.Length);
+#else
+                string sep = GenerateSpaceString(l - firstString.Length);
+#endif
+                return firstString + sep;
+            }
+
+            int itemIndex = -1;
+            StringBuilder[] builders = GetStringBuilders(x);
+            for (var i = l - stringsLength; stringsLength < l; i--)
+            {
+                itemIndex++;
+                builders[itemIndex].Append(' ');
+                stringsLength++;
+
+                if (itemIndex == arrayLength - 2)
+                {
+                    itemIndex = -1;
+                }
+            }
+
+#if UseNative
+            StringBuilder resultStringBuilder = new StringBuilder();
+            for(int i = 0; i < arrayLength; i++)
+            {
+                resultStringBuilder.Append(builders[i]);
+            }
+            return resultStringBuilder.ToString();      
+#else
+            return builders.Aggregate((x, y) => x.Append(y)).ToString();
+#endif
+        }
+    }
+}
